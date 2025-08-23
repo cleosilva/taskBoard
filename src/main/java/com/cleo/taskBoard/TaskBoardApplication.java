d980c8b00ff406d5c61fb2d7a4782fb017eea6b7
Cleo Silva <cleo_silva1906@hotmail.com>
2025-08-23 17:19:11 -0300

    Initial commit

---
+++ src/main/java/com/cleo/taskBoard/TaskBoardApplication.java
@@ -0,0 +1,13 @@
+package com.cleo.taskBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskBoardApplication.class, args);
+	}
+
+}
