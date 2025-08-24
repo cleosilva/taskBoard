a0051431ae24e2d3da92cca9fac9816ec6ba3202
Cleo Silva <cleo_silva1906@hotmail.com>
2025-08-24 20:08:12 -0300

    feat: Create Lane entity

--- src/main/java/com/cleo/taskBoard/model/Lane.java
+++ src/main/java/com/cleo/taskBoard/model/Lane.java
@@ -1,2 +1,25 @@
-package com.cleo.taskBoard.model;public class Lane {
+package com.cleo.taskBoard.model;
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "lane")
+public class Lane {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    private Integer position;
+    //@ManyToOne: Define um relacionamento muitos-para-um com a entidade Board.
+    // Board associada só será carregada do banco de dados quando você explicitamente a acessar, o que melhora a performance.
+    @ManyToOne(fetch = FetchType.LAZY)
+    // Especifica a coluna que será a chave estrangeira (board_id) na tabela lane.
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
 }
