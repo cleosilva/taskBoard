e99d359a17acb8daa2d3decb1ca578178d24b559
Cleo Silva <cleo_silva1906@hotmail.com>
2025-08-24 20:24:15 -0300

    feat: Implement repository classes

---
+++ src/main/java/com/cleo/taskBoard/repository/LaneRepository.java
@@ -0,0 +1,6 @@
+package com.cleo.taskBoard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LaneRepository extends JpaRepository {
+}
---
+++ src/main/java/com/cleo/taskBoard/repository/TaskRepository.java
@@ -0,0 +1,6 @@
+package com.cleo.taskBoard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository {
+}
